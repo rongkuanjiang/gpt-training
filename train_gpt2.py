9a5214514998b1e15465694bbe631df7f45797b2
rongkuan jiang <r75jiang@uwaterloo.ca>
2025-10-29 20:15:47 -0400

    timingand batch size change

--- train_gpt2.py
+++ train_gpt2.py
@@ -185,15 +185,16 @@ elif hasattr(torch.backends, 'mps') and torch.mps.is_available():
 print(f"using device: {device}")
 #model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig()) # generates random weights
-print('yo')
+model.to(device)
 model.train()
 
 num_return_sequences = 5
 max_length = 30
+print('model on:', next(model.parameters()).device)  # should say cuda:0
 
 
 # model.eval()
-model.to(device)
+
 enc = tiktoken.get_encoding('gpt2')
 
 
@@ -222,39 +223,43 @@ class DataLoaderLite:
         
         return x, y
         
-train_loader = DataLoaderLite(B=4, T=32)
+train_loader = DataLoaderLite(B=16, T=1024)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1 - t0)*1000
+    print(f"step {i}, time: {dt}ms, loss: {loss.item()}")
 # import sys; sys.exit(0)
 
-tokens = enc.encode("Behold, ")
-tokens = torch.tensor(tokens, dtype=torch.long)
-tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
-x = tokens.to(device)
+# tokens = enc.encode("Behold, ")
+# tokens = torch.tensor(tokens, dtype=torch.long)
+# tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+# x = tokens.to(device)
 
-torch.manual_seed(42)
+# torch.manual_seed(42)
 
-while x.size(1) < max_length:
-    with torch.no_grad():
-        logits, _ = model(x)
-        logits = logits[:, -1, :]
-        probs = F.softmax(logits, dim=-1)
-        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
-        ix = torch.multinomial(topk_probs, 1)
-        xcol = torch.gather(topk_indices, -1, ix)
-        x = torch.cat((x, xcol), dim=1)
+# while x.size(1) < max_length:
+#     with torch.no_grad():
+#         logits, _ = model(x)
+#         logits = logits[:, -1, :]
+#         probs = F.softmax(logits, dim=-1)
+#         topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+#         ix = torch.multinomial(topk_probs, 1)
+#         xcol = torch.gather(topk_indices, -1, ix)
+#         x = torch.cat((x, xcol), dim=1)
 
 
-for i in range(num_return_sequences):
-    tokens = x[i, :max_length].tolist()
-    print(tokens)
-    decoded = enc.decode(tokens)
-    print(">", decoded)
+# for i in range(num_return_sequences):
+#     tokens = x[i, :max_length].tolist()
+#     print(tokens)
+#     decoded = enc.decode(tokens)
+#     print(">", decoded)
